260fd0f20bd9c2d88165d0465c681558a7130dd0
yoodz <904201050@qq.com>
2025-03-07 09:07:15 +0800

    更新分页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 "use client"
 import { useEffect, useState } from "react";
 
+const PAGE_SIZE = 20
 export default function Home() {
   const [posts, setPosts] = useState({
     results: [],
-    page: 1
+    page: 1,
+    total: 0
   })
 
   async function getData(page: number) {
-    const data = await fetch(`https://blogapi.afunny.top/.netlify/functions/get-rss?page=${page}&page_size=20`)
+    const data = await fetch(`https://blogapi.afunny.top/.netlify/functions/get-rss?page=${page}&page_size=${PAGE_SIZE}`)
     const res = await data.json()
     setPosts(res)
+  }
+
+  const nextPage = () => {
+    const next = +posts.page * PAGE_SIZE
+    if (next < posts.total) {
+      getData(+posts.page + 1)
+    } else {
+      console.log('没有下一页了')
+    }
   }
 
   useEffect(() => {
     getData(1)
   }, [])
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
@@ -30,7 +42,7 @@ export default function Home() {
 
           </div>
         })}
-        <div onClick={() => getData(+posts.page + 1)}>更多</div>
+        <div className="cursor-pointer text-blue-400" onClick={nextPage}>更多</div>
       </main>
     </div>
   );
