8226f342721998316f588e60970011cf8878c37f
yoodz <904201050@qq.com>
2025-03-10 09:14:34 +0800

    不缓存请求

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ClientPage from './clientPage'
 
 async function getData(page: number) {
   try {
-    const data = await fetch(`https://blogapi.afunny.top/.netlify/functions/get-rss?page=${page}&page_size=20`)
+    const data = await fetch(`https://blogapi.afunny.top/.netlify/functions/get-rss?page=${page}&page_size=20`, { cache: 'no-store' })
     return await data.json()
   } catch (e) {
     console.log(e, 'page-8')
@@ -12,7 +12,6 @@ async function getData(page: number) {
 export default async function Home() {
   // 首屏服务端请求
   const initialData = await getData(1)
-
   return <ClientPage initialData={initialData} />
 
 }
