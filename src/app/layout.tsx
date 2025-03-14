d51a241ad55709b5a713750fb63897de61897f99
yoodz <904201050@qq.com>
2025-03-14 08:02:01 +0800

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-X8RCWS4C9Z" />
     </html>
   );
 }
