25e1ad915f5814e2567f061404d8bc78cacee561
yoodz <904201050@qq.com>
2025-04-05 15:13:40 +0800

    更新接口地址

--- src/app/clientPage.tsx
+++ src/app/clientPage.tsx
@@ -37,7 +37,7 @@ import { formatTime } from '@/lib/utils'
 //   initialData: {
 //     total: number
 //     page: number
-//     results: {
+//     result: {
 //       title: string
 //       link: string
 //       hostname: string
@@ -50,7 +50,7 @@ import { formatTime } from '@/lib/utils'
 // }
 
 interface IResult {
-  _id: string
+  id: string
   title: string
   link: string
   hostname: string
@@ -60,8 +60,8 @@ interface IResult {
 const PAGE_SIZE = 20
 export default function Home() {
   const form = useForm()
-  const [posts, setPosts] = useState({ total: 0, page: 0, config: { updateAt: '' }, results: [] })
-  const [config, setConfig] = useState({ updateAt: '' })
+  const [posts, setPosts] = useState({ total: 0, page: 0, config: { updateAt: '' }, result: [] })
+  const [config, setConfig] = useState({ value: '' })
   const [totalRss, setTotalRss] = useState(0)
   const [wait, setWait] = useState(0)
   const { totalPage, currentPage } = useMemo(() => {
@@ -73,10 +73,10 @@ export default function Home() {
 
   async function getData(page: number) {
     try {
-      const data = await fetch(`https://blogapi.afunny.top/.netlify/functions/get-rss?page=${page}&page_size=${PAGE_SIZE}`)
+      const data = await fetch(`https://cf.afunny.top/article?page=${page}&page_size=${PAGE_SIZE}`)
       const res = await data.json()
       setPosts(res)
-      if (page === 1) {
+      if (+page === 1) {
         const { config, totalRss } = res || {}
         setConfig(config)
         setTotalRss(totalRss)
@@ -94,9 +94,10 @@ export default function Home() {
   }
 
   const handleClick = (item: IResult) => {
-    const { _id, link } = item || {}
+    const { id, link } = item || {}
+    console.log(item, 'clientPage-99')
     try {
-      fetch(`https://blogapi.afunny.top/.netlify/functions/rss-activity?id=${_id}`)
+      fetch(`https://cf.afunny.top/article/pv?id=${id}`)
     } catch (error) {
       console.log(error, 'clientPage-92')
     } finally {
@@ -176,7 +177,7 @@ export default function Home() {
             <div className="mr-3 whitespace-nowrap">Blog News</div>
             <svg onClick={() => window.open('https://github.com/yoodz/blog-news')} fill="currentColor" height="1em" stroke="currentColor" strokeWidth="0" viewBox="0 0 512 512" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"></path></svg>
           </div>
-          <span className="text-xs font-normal flex-1 absolute opacity-60 mr-1 md:relative md:ml-3">最近更新：{config?.updateAt || '-'} | 当前收录：{totalRss || '-'}个</span>
+          <span className="text-xs font-normal flex-1 absolute opacity-60 mr-1 md:relative md:ml-3">最近更新：{config?.value || '-'} | 当前收录：{totalRss || '-'}个</span>
         </div>
         <Dialog>
           <DialogTrigger asChild><Button className="cursor-pointer" variant="outline" onClick={initAwaitCount}>提交RSS</Button></DialogTrigger>
@@ -233,7 +234,7 @@ export default function Home() {
       </div>
       <div className="text-md text-gray-500 my-4">一个基于RSS的Blog News博客聚合项目，每天自动抓取感兴趣的博客文章, 及时获取博客动态。</div>
       <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
-        {posts?.results?.map(item => {
+        {posts?.result?.map(item => {
           const { title, hostname, pubDate } = item || {}
           return <div className="w-full cursor-pointer hover:bg-blue-50 p-2 rounded-lg" key={title} onClick={() => handleClick(item)}>
             <div className="flex">
@@ -245,7 +246,7 @@ export default function Home() {
             </div>
           </div>
         })}
-        {(!posts?.results?.length) && Array.from({ length: 20 }).map((item, idx) => {
+        {(!posts?.result?.length) && Array.from({ length: 20 }).map((item, idx) => {
           return <div className="flex items-center w-full mt-3 mb-5" key={idx}>
             <div className="w-full">
               <Skeleton className="h-4 w-4/5 mb-2" />
